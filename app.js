c4dccc0e56536fae85fcbe8e963d7b06ce3750f9
Smik Lakhani <smiklakhani@gmail.com>
2017-12-18 13:34:36 -0500

    Adding sample logging with Winston to illustrate logging

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  logger.log('error', 'Requested resource does not exist.');
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
+var logger = require('winston');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  logger.log('info', 'index route was requested');
   res.render('index', { title: 'Express' });
 });
 
